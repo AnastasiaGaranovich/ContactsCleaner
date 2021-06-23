9db4ceb0ed631389ef71ccd3003e90a2934feb73
AnastasiaGaranovich <nastyamadness@gmail.com>
2021-06-23 14:54:22 +0300

    Create project, add gitignore

---
+++ ContactCleaner/ContactCleaner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactCleaner
+//
+//  Created by Анастасия Гаранович on 23.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
